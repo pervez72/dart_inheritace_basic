0c69616ac8f966288fe0e44c96480de2f26b76b9
pervez-hasan <phparvez71@gmail.com>
2023-01-08 11:06:55 +0600

    overrideed sum mthod

--- oop_advanced/bin/first.dart
+++ oop_advanced/bin/first.dart
@@ -4,7 +4,12 @@ class mysoon extends me{ // all data and method tranferd mysoon
   void namess(){
     print('he is not a bron.up comeing');
   }
+  @override //ja ar father class er sum() method er kotha sunbe nh
+  void sum(){
+    print("override method");
 
+
+  }
 
 
 
